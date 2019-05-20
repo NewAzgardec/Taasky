b4c56427c5d5ba959c18d8a8b87feed6a173bb10
Masha Senkevich <masha13242@gmail.com>
2019-05-20 17:42:34 +0300

    write/read methods

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -16,23 +16,23 @@ import java.util.regex.Pattern;
 
 public class GUI extends JFrame {
 
-    public JLabel jLabelClock;
+    private static final String DATA_FILE=("C:\\Users\\Masha\\IntelliJIDEAProjects\\AipLaba\\src\\data.txt");
+    private static final String IMAGE_PATH=("C:\\Users\\Masha\\IntelliJIDEAProjects\\AipLaba\\src\\config\\icon1.png");
 
     private String TIP_UPDATE = "Click to change your theme";
     private String TIP_TEXTFIELD = "Enter task";
     private String TIP_ADD = "Click to add";
     private String TIP_DELETE = "Click to remove";
     private String TIP_EDIT = "Click to edit";
-    private String langsProp;
     private String defaultLang;
     private Set<String> langSet;
 
+    public JLabel jLabelClock;
     private JFrame frame;
     private JMenuBar menu;
     private final JTextField textField;
     private final JLabel label;
     private Properties props;
-    private ClockThread ct;
     private JPanel addRemovePanel;
     private JButton updateLookAndFeelButton;
 
@@ -99,6 +99,7 @@ public class GUI extends JFrame {
         });
 
         JScrollPane tasksScrollPane = new JScrollPane(listBox);
+        readTask();
         DefaultListCellRenderer renderer = (DefaultListCellRenderer) listBox.getCellRenderer();
         renderer.setHorizontalAlignment(JLabel.CENTER);
 
@@ -114,7 +115,7 @@ public class GUI extends JFrame {
         topPanel.add(textField, BorderLayout.NORTH);
         topPanel.add(tasksScrollPane);
 
-        Image icon = new ImageIcon("C:\\Users\\Masha\\IntelliJIDEAProjects\\AipLaba\\src\\config\\icon1.png").getImage();
+        Image icon = new ImageIcon(IMAGE_PATH).getImage();
 
         String FRAME = "Taasky";
         frame = new JFrame(FRAME);
@@ -128,6 +129,15 @@ public class GUI extends JFrame {
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         frame.setResizable(false);
+
+        WindowListener listener = new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent w) {
+                writeTask();
+                System.exit(0);
+            }
+        };
+        frame.addWindowListener(listener);
 
         updateLookAndFeelButton.addActionListener(
                 new UpdateLookAndFeelAction(frame, list)
@@ -146,7 +156,7 @@ public class GUI extends JFrame {
         jLabelClock = new JLabel();
         jLabelClock.setFont(new Font("Comin Sans MS", Font.BOLD, 16));
         add(jLabelClock);
-        ct = new ClockThread(GUI.this);
+        ClockThread ct = new ClockThread(GUI.this);
         jLabelClock.setHorizontalAlignment(SwingConstants.CENTER);
         jLabelClock.setVerticalAlignment(SwingConstants.CENTER);
     }
@@ -166,7 +176,7 @@ public class GUI extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        langsProp = props.getProperty(PROP_LANGS);
+        String langsProp = props.getProperty(PROP_LANGS);
         defaultLang = props.getProperty(PROP_LANGS_DEFAULT);
         langSet = new TreeSet<>(Arrays.asList(langsProp.split(",")));
     }
@@ -185,7 +195,8 @@ public class GUI extends JFrame {
         JMenuItem miExit = new JMenuItem();
         miExit.putClientProperty(MyLocale.LOCALIZATION_KEY, "menu.file.exit");
         mFile.add(miExit);
-        miExit.addActionListener(e -> System.exit(0));
+        miExit.addActionListener(e -> {writeTask();System.exit(0);
+        });
 
         ButtonGroup buttonGroup = new ButtonGroup();
         for (final String lang : languages) {
@@ -197,6 +208,34 @@ public class GUI extends JFrame {
                 menuLangs.setSelected(true);
             }
             menuLangs.addActionListener(e -> applyLocale(lang));
+        }
+    }
+
+    private void writeTask(){
+        try{
+            OutputStream f = new FileOutputStream(DATA_FILE, false);
+            OutputStreamWriter writer = new OutputStreamWriter(f);
+            BufferedWriter out = new BufferedWriter(writer);
+            for(int i = 0; i < names.size(); i++)
+            {
+                out.write(names.get(i)+"\n");
+                out.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void readTask(){
+
+        try (BufferedReader BR = new BufferedReader(new FileReader(new File(DATA_FILE)))) {
+            String item;
+            while ((item = BR.readLine()) != null) {
+                names.addElement(item);
+            }
+            listBox.setModel(names);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
